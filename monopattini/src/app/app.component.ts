2f8e7ab3b9a67f66ed9fdd96f914b7620a758f61
brancatellimatteo <brancatelli.matteo@iisgalvanimi.edu.it>
2019-05-21 17:18:06 +0000

    visible/not visible, comp create

--- monopattini/src/app/app.component.ts
+++ monopattini/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monopattini';
+  public askIsVisible : boolean;
+  public loginIsVisible : boolean;
+  public signupIsVisible : boolean;
+
+  constructor(public http: HttpClient) {
+    this.askIsVisible = true;
+  }
+
+  public makeLoginVisible(): void {
+    this.loginIsVisible = !this.loginIsVisible;
+    this.askIsVisible = false;
+  }
+
+  public makeSignupVisible(): void {
+    this.signupIsVisible = !this.signupIsVisible;
+    this.askIsVisible = false;
+  }
 }
--- monopattini/src/app/app.module.ts
+++ monopattini/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //Bootstrap
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http'; //client
+import { AgmCoreModule } from '@agm/core';
+//import { MappaComponent } from './mappa/mappa.component';
+import { SignupComponent } from './signup/signup.component'; //Mappe
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    //MappaComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCzUI8LYmnHPyFrtRT8Q8IEREZfOygUl-U' //walter
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
